32aa230fb50c5ea559439c029b7ecfcd34aa5283
furyArtanis <l1974823308@163.com>
2020-11-15 10:48:25 +0800

    路由配置

---
+++ src/pages/cart.vue
@@ -0,0 +1,18 @@
+<template>
+  <order-header></order-header>
+  cart
+  <nav-footer></nav-footer>
+</template>
+
+<script>
+import OrderHeader from "@/components/OrderHeader";
+import NavFooter from "@/components/NavFooter";
+export default {
+  name: "index",
+  components: {NavFooter, OrderHeader}
+}
+</script>
+
+<style scoped>
+
+</style>
